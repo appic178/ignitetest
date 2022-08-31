9f9589ba8da56fae7091580349d9691b859ba7f2
appic178 <112542662+appic178@users.noreply.github.com>
2022-08-31 12:37:09 +0500

    Add files via upload

---
+++ igniteTest/iosdeveloperapp/igniteTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  igniteTest
+//
+//  Created by fstech on 8/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is the main branch")
+        print("make a two new branch")
+        print(" add branch")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
